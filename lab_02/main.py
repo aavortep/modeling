c416dfcc91c4172c478e7115665457c9800e8988
Anna Petrova <nura.alexevna@yandex.ru>
2022-10-28 23:46:44 +0300

    finish program

--- lab_02/main.py
+++ lab_02/main.py
@@ -6,6 +6,8 @@ import sys  # sys нужен для передачи argv в QApplication
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QTableWidgetItem
 import inputs
+import results
+
 TIME_DELTA = 1e-3
 EPS = 1e-5
 
@@ -45,8 +47,128 @@ class Inputs(QtWidgets.QMainWindow, inputs.Ui_MainWindow):
                     item = str(round(random.random(), 4))
                     self.matrix.setItem(i, j, QTableWidgetItem(item))
 
+    def get_table(self):
+        matrix = []
+        for i in range(self.states_num):
+            row = []
+            for j in range(self.states_num):
+                row.append(self.matrix.item(i, j).text())
+            matrix.append(row)
+        return matrix
+
+    def _dp(self, matrix, probabilities):
+        res = []
+        n = len(matrix)
+        for i in range(n):
+            summ = 0
+            for j in range(n):
+                if i == j:
+                    sum_i = 0
+                    for t in range(n):
+                        sum_i += float(matrix[i][t])
+
+                    summ += probabilities[j] * (-1 * sum_i + float(matrix[i][i]))
+                else:
+                    summ += probabilities[j] * float(matrix[j][i])
+            res.append(TIME_DELTA * summ)
+        return res
+
+    def get_stab_time(self, matrix, start_probabilities):
+        n = len(matrix)
+        current_time = 0
+        current_probabilities = start_probabilities.copy()
+        stabilization_times = [0 for i in range(n)]
+        stabilization_p = [0 for i in range(n)]
+        prev_probabilities = []
+        for i in range(n):
+            prev_probabilities.append([])
+        x = []
+        counter = 0
+        prev_dp = self._dp(matrix, current_probabilities)
+        while not all(stabilization_times):
+            while counter < 100:
+                curr_dp = self._dp(matrix, current_probabilities)
+                for i in range(n):
+                    prev_probabilities[i].append(current_probabilities[i])
+                    current_probabilities[i] += curr_dp[i]
+                counter += 1
+                x.append(current_time)
+                current_time += TIME_DELTA
+            for i in range(n):
+                if not stabilization_times[i] and abs(prev_dp[i] - curr_dp[i]) < EPS and abs(curr_dp[i]) < EPS:
+                    stabilization_times[i] = current_time - TIME_DELTA * 30
+                    stabilization_p[i] = current_probabilities[i]
+            counter = 0
+            prev_dp = curr_dp
+
+        counter = 0
+        while counter < 100:
+            curr_dp = self._dp(matrix, current_probabilities)
+            for i in range(n):
+                prev_probabilities[i].append(current_probabilities[i])
+                current_probabilities[i] += curr_dp[i]
+            counter += 1
+            x.append(current_time)
+            current_time += TIME_DELTA
+        fig, ax = plt.subplots()
+
+        for i in range(n):
+            ax.plot(x, prev_probabilities[i], label='S' + str(i+1))
+            ax.scatter(stabilization_times[i], stabilization_p[i], color='orange', s=40, marker='o')
+        ax.legend()
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Probabilities')
+        plt.show()
+        return stabilization_times
+
     def compute(self):
-        #
+        matrix = self.get_table()
+
+        matrix = numpy.array(matrix)
+        n = len(matrix)
+        coeff_matrix = numpy.zeros((n, n))
+
+        for state in range(n - 1):
+            for col in range(n):
+                coeff_matrix[state, state] -= float(matrix[state, col])
+            for row in range(n):
+                coeff_matrix[state, row] += float(matrix[row, state])
+
+        for state in range(n):
+            coeff_matrix[n - 1, state] = 1
+
+        res = [0 for i in range(n)]
+        res[n - 1] = 1
+        augmentation_matrix = numpy.array(res)
+
+        probs = numpy.linalg.solve(coeff_matrix, augmentation_matrix)
+
+        start_probabilities = [0] * n
+        start_probabilities[0] = 1
+        stab_time = self.get_stab_time(matrix, start_probabilities)
+
+        self.window = Results(probs, stab_time)
+        self.window.show()
+
+
+class Results(QtWidgets.QMainWindow, results.Ui_MainWindow):
+    def __init__(self, probs, stab_time):
+        super().__init__()
+        self.setupUi(self)
+
+        self.probs_table.setColumnCount(len(probs))
+        self.probs_table.setRowCount(1)
+
+        self.stab_table.setColumnCount(len(stab_time))
+        self.stab_table.setRowCount(1)
+
+        for i in range(len(probs)):
+            prob = str(round(probs[i], 4))
+            self.probs_table.setItem(0, i, QTableWidgetItem(prob))
+
+        for i in range(len(stab_time)):
+            stab = str(round(stab_time[i], 4))
+            self.stab_table.setItem(0, i, QTableWidgetItem(stab))
 
 
 if __name__ == '__main__':
